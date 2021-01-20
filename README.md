ee1caf4478766e9e4e13c759f2b25812f32c663d
jpaulohe4rt <jpaulo.on@gmail.com>
2021-01-20 12:30:51 -0300

    wip

---
+++ README.md
@@ -0,0 +1,5 @@
+# Xaropinho BOT
+
+bot que pega tweets com voz do xaropinho e responde RAPÁÁÁZ.
+
+funcionando no twitter.com/xaropinhobot
---
+++ app.py
@@ -0,0 +1,25 @@
+import tweepy
+from time import sleep
+from datetime import datetime
+from credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+search = 'voz do xaropinho'
+today = datetime.today().strftime('%Y-%m-%d')
+
+for tweet in tweepy.Cursor(api.search, search, since = today).items():
+    try:
+        if tweet.author.screen_name != 'xaropinhobot' and not tweet.retweeted:
+                print("@" + tweet.user.screen_name)
+                tweet.favorite()
+                reply = 'RAPÁÁÁZ ' + 'www.youtube.com/watch?v=whns12cw4-w&ab_channel=XaropinhoBot' 
+                api.update_status(reply, tweet.id, auto_populate_reply_metadata=True)
+                sleep(90)
+    except tweepy.error.TweepError as e:
+        pass
+    except StopIteration:
+        break
